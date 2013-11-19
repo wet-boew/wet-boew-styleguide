9ef4f74b7c5ca880d20850a52fc9c9e9c60be234
Laurent Goderre <laurent.goderre@gmail.com>
2013-11-19 13:50:07 -0500

    Fixed the push to gh-pages command

--- travis.sh
+++ travis.sh
@@ -5,4 +5,4 @@ git config --global user.email "wet.boew.bot@gmail.com"
 git config --global user.name "Web Experience Toolkit Bot"
 
 git checkout -fb gh-pages
-git push -f https://GH_TOKEN@github.com/wet-boew/wet-boew-styleguide.git > /dev/null 2>&1 || echo "Error publishing to gh-pages" && exit 1
+git push -f https://${GH_TOKEN}@github.com/wet-boew/wet-boew-styleguide.git gh-pages > /dev/null 2>&1 || (echo "Error pushing to gh-pages" && exit 1)
