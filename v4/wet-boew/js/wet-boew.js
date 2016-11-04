85300c419b1391562e15c23e3555bf7a1faf1fc9
Web <wet.boew.bot@gmail.com>
2016-11-04 19:02:20 +0000

    Travis build 322

--- v4/wet-boew/js/wet-boew.js
+++ v4/wet-boew/js/wet-boew.js
@@ -1,7 +1,7 @@
 /*!
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
- * v4.0.23-development - 2016-11-03
+ * v4.0.23-development - 2016-11-04
  *
  *//*! Modernizr (Custom Build) | MIT & BSD */
 /* Modernizr (Custom Build) | MIT & BSD
